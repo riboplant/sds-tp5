b972e6e1857f6c99a56592d2d4dffdc95a6db50a
martinAleB <martbarr@gmail.com>
2025-10-22 16:46:29 -0300

    chore: initialize simulations engine

---
+++ simulations/src/main/java/ar/edu/itba/sds/tp5/simulations/App.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.sds.tp5.simulations;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ simulations/src/test/java/ar/edu/itba/sds/tp5/simulations/AppTest.java
@@ -0,0 +1,19 @@
+package ar.edu.itba.sds.tp5.simulations;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
